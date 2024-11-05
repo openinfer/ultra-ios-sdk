5b515be7e7171fcddae9c11f46b05e12251b5605
Roman <seadller@gmail.com>
2024-11-05 16:34:01 +0200

    Implement visual vc

--- Sources/CryptonetPackage/CryptonetPackage.swift
+++ Sources/CryptonetPackage/CryptonetPackage.swift
@@ -189,12 +189,21 @@ private extension CryptonetPackage {
     }
 }
 
-// Visual testing
-public extension CryptonetPackage {
+public class CryptonetViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func runCamera(viewController: UIViewController) {
         print("Test")
         let cameraVc = UIImagePickerController()
         cameraVc.sourceType = UIImagePickerController.SourceType.camera
+        cameraVc.delegate = self
         viewController.present(cameraVc, animated: true, completion: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            //save image
+            //display image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
---
+++ Sources/CryptonetPackage/CryptonetVisual.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+public class CryptonetVisual: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func runCamera(viewController: UIViewController) {
+        print("Test")
+        let cameraVc = UIImagePickerController()
+        cameraVc.sourceType = UIImagePickerController.SourceType.camera
+        cameraVc.delegate = self
+        viewController.present(cameraVc, animated: true, completion: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            //save image
+            //display image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}

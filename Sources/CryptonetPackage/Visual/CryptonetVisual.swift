fbbc73cb13eee5b456aacf3b2495d5ac3cef6168
Roman <seadller@gmail.com>
2024-11-05 17:13:15 +0200

    Implement custom camera session for vc

--- Sources/CryptonetPackage/Visual/CryptonetVisual.swift
+++ Sources/CryptonetPackage/Visual/CryptonetVisual.swift
@@ -1,28 +1,55 @@
 import UIKit
+import AVFoundation
+import CoreMedia
 
-public class CryptonetVisual: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+public class CryptonetVisual: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    @IBOutlet weak var greetings: UILabel!
+    @IBOutlet weak var videoFrame: UIView!
+    @IBOutlet weak var videoContainer: UIView!
+    
+    private let session = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        runCamera()
     }
     
-    public func runCamera() {
-        print("Test")
-        let cameraVc = UIImagePickerController()
-        cameraVc.sourceType = UIImagePickerController.SourceType.camera
-        cameraVc.delegate = self
-        self.present(cameraVc, animated: true, completion: nil)
+    public override func viewDidAppear(_ animated: Bool) {
+        setupCamera()
     }
     
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            //save image
-            //display image
-        }
+    func setupCamera() {
+        session.sessionPreset = .photo
         
-        picker.dismiss(animated: true, completion: nil)
+        let possition: AVCaptureDevice.Position = .front
+        
+        let cameraType: AVCaptureDevice.DeviceType = .builtInWideAngleCamera
+        guard let captureDevice = AVCaptureDevice.default(cameraType, for: .video, position: possition) else { return }
+
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer?.videoGravity = .resizeAspectFill
+            previewLayer?.contentsGravity = .resizeAspectFill
+            previewLayer?.frame = videoFrame.layer.bounds
+            videoFrame.layer.addSublayer(previewLayer!)
+            
+            let output = AVCaptureVideoDataOutput()
+            output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            session.addOutput(output)
+            startSession()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func startSession() {
+        if !session.isRunning {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.session.startRunning()
+            }
+        }
     }
 }

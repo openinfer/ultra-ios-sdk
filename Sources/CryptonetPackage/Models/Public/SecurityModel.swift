fef36e8ad8a98f00b7bb57bb984d179404ced8f2
Roman <seadller@gmail.com>
2024-11-25 18:20:40 +0200

    Update model init

--- Sources/CryptonetPackage/Models/Public/SecurityModel.swift
+++ Sources/CryptonetPackage/Models/Public/SecurityModel.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 public struct SecurityModel {
-    let model: String // UIDevice.current.model
-    let sceenBounds: CGRect // UIScreen.main.bounds
-    let orientation: UIDeviceOrientation // UIDevice.current.orientation
-    let processorCount: Int // ProcessInfo().processorCount
-    let battertLevel: Float // UIDevice.current.batteryLevel
-    let languageCode: String // Locale.current.languageCode
+    let model: String = UIDevice.current.model
+    let sceenBounds: CGRect = UIScreen.main.bounds
+    let orientation: UIDeviceOrientation = UIDevice.current.orientation
+    let processorCount: Int = ProcessInfo().processorCount
+    let battertLevel: Float = UIDevice.current.batteryLevel
+    let languageCode: String? = Locale.current.languageCode
 }

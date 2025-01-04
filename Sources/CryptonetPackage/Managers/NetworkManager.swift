8fb17ff7dd515ea399b07c79469fdb304824e06f
Roman <seadller@gmail.com>
2025-01-04 12:57:21 +0200

    Update start sdk

--- Sources/CryptonetPackage/Managers/NetworkManager.swift
+++ Sources/CryptonetPackage/Managers/NetworkManager.swift
@@ -1,7 +1,81 @@
-//
-//  NetworkManager.swift
-//  UltraPackage
-//
-//  Created by Roman on 04.01.2025.
-//
+import UIKit
+import Alamofire
+import ProgressHUD
 
+final class NetworkManager {
+    
+    enum SessionFlow: String {
+        case enroll = "ENROLL"
+        case predict = "VERIFY"
+    }
+    
+    static let shared = NetworkManager()
+    
+    var baseURL = "https://api-orchestration-privateid.uberverify.com/"
+    var redirectURL = "https://privateid.uberverify.com"
+    
+    private init() { }
+    
+    func getSessionToken(with uuid: String = UUID().uuidString, type: SessionFlow = .predict, token: @escaping (String?) -> Void) {
+        guard let url = URL(string: "\(baseURL)v2/verification-session") else {
+            token(nil)
+            return
+        }
+        
+        let params: [String : Any] = [
+            "type" : type.rawValue,
+            "redirectURL" : redirectURL,
+            "deviceInfo" : [:],
+            "callback" : [
+                "url" : redirectURL,
+                "headers" : [:]
+            ],
+            "uuid": uuid
+        ]
+        
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "skip-auth",
+            "x-api-key": "0000000000000000test"
+        ]
+        
+        AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers)
+            .validate()
+            .responseDecodable(of: ResponseModel.self) { response in
+                switch response.result {
+                case .success(let response):
+                    token(response.sessionId)
+                case .failure(let error):
+                    print("Failed: \(error.localizedDescription)")
+                    ProgressHUD.failed("Internal server error")
+                    token(nil)
+                }
+            }
+    }
+    
+    func getPublicKey(publicKey: @escaping (String?) -> Void) {
+        guard let url = URL(string: "\(baseURL)public-key") else {
+            publicKey(nil)
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "skip-auth",
+            "x-api-key": "0000000000000000test"
+        ]
+        
+        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+            .validate()
+            .responseDecodable(of: ResponseModel.self) { response in
+                switch response.result {
+                case .success(let response):
+                    publicKey(response.publicKey)
+                case .failure(let error):
+                    print("Failed: \(error.localizedDescription)")
+                    ProgressHUD.failed("Internal server error")
+                    publicKey(nil)
+                }
+            }
+    }
+}
--- Sources/CryptonetPackage/Managers/UltraPackage.swift
+++ Sources/CryptonetPackage/Managers/UltraPackage.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 enum CryptonetError: Error {
     case noJSON
@@ -11,28 +12,46 @@ public class UltraPackage {
     
     public init() {}
     
-    public func start(path: String, token: String, publicKey: String, browser: String, securityModel: SecurityModel) -> Bool {
-        
-        // todo:
-        let baseURL = "https://api-orchestration-privateid.uberverify.com/"
-        
-        let settings = """
-        {
-          "collections": {
-            "default": {
-              "named_urls": {
-                "base_url": "\(baseURL)" } } },
-          "public_key": "\(publicKey)",
-          "session_token": "\(token)"
-        }
-        """
-        CryptonetManager.shared.sessionToken = token
-        CryptonetManager.shared.publicKey = publicKey
-        CryptonetManager.shared.selectedBrowser = browser
+    public func start(path: String, token: String?, publicKey: String?, browser: String?, securityModel: SecurityModel, finished: @escaping (Bool) -> Void) {
         CryptonetManager.shared.initializeLib(path: NSString(string: path))
-        let result = CryptonetManager.shared.initializeSession(settings: NSString(string: settings))
-        let securityCheck = self.checkSecurityConditions(securityModel: securityModel)
-        return result && securityCheck
+        CryptonetManager.shared.selectedBrowser = browser ?? "chrome"
+        
+        NetworkManager.shared.getSessionToken { newToken in
+            guard let newToken = newToken else {
+                ProgressHUD.failed("Empty session token")
+                finished(false)
+                return
+            }
+            
+            CryptonetManager.shared.sessionToken = token ?? newToken
+            NetworkManager.shared.getPublicKey { newPublicKey in
+                guard let newPublicKey = newPublicKey else {
+                    ProgressHUD.failed("Empty public key")
+                    finished(false)
+                    return
+                }
+                
+                CryptonetManager.shared.publicKey = publicKey ?? newPublicKey
+                
+                let finalToken = CryptonetManager.shared.sessionToken ?? newToken
+                let finalKey = CryptonetManager.shared.publicKey ?? newPublicKey
+                
+                let settings = """
+                {
+                  "collections": {
+                    "default": {
+                      "named_urls": {
+                        "base_url": "\(NetworkManager.shared.baseURL)v2/verification-session" } } },
+                  "public_key": "\(finalKey)",
+                  "session_token": "\(finalToken)"
+                }
+                """
+                
+                let securityCheck = self.checkSecurityConditions(securityModel: securityModel)
+                let result = CryptonetManager.shared.initializeSession(settings: NSString(string: settings))
+                finished(result && securityCheck)
+            }
+        }
     }
     
     public func runVisual(on viewController: UIViewController) {
@@ -40,13 +59,13 @@ public class UltraPackage {
         let vc = storyboard.instantiateViewController(withIdentifier: "InstructionsViewController")
         viewController.navigationController?.pushViewController(vc, animated: true)
     }
-
+    
     func checkSecurityConditions(securityModel: SecurityModel) -> Bool {
         return securityModel.model == UIDevice.current.model &&
-               securityModel.sceenBounds == UIScreen.main.bounds &&
-               securityModel.orientation == UIDevice.current.orientation &&
-               securityModel.processorCount == ProcessInfo().processorCount &&
-               securityModel.battertLevel == UIDevice.current.batteryLevel &&
-               securityModel.languageCode == Locale.current.languageCode
+        securityModel.sceenBounds == UIScreen.main.bounds &&
+        securityModel.orientation == UIDevice.current.orientation &&
+        securityModel.processorCount == ProcessInfo().processorCount &&
+        securityModel.battertLevel == UIDevice.current.batteryLevel &&
+        securityModel.languageCode == Locale.current.languageCode
     }
 }

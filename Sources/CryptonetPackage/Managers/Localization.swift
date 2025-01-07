278731636ae713a58e3080d952c7aed0db362bbe
Roman <seadller@gmail.com>
2025-01-07 14:51:30 +0200

    Implement localisation files in SPM

---
+++ Sources/CryptonetPackage/Managers/Localization.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    func localized(with arguments: CVarArg...) -> String {
+        let arguments = arguments.compactMap { $0 }
+        let localizedString = localized.replacingOccurrences(of: "%s", with: "%@")
+        return String(format: localizedString, arguments: arguments)
+    }
+}

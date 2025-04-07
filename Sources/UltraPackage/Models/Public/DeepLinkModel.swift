409068ebc5a4086b6da3492d54f84f5305e3b5d0
Roman <seadller@gmail.com>
2025-04-07 16:45:06 +0300

    Implement public init for class model

--- Sources/UltraPackage/Models/Public/DeepLinkModel.swift
+++ Sources/UltraPackage/Models/Public/DeepLinkModel.swift
@@ -1,10 +1,24 @@
 import Foundation
 
-public struct DeeplinkData {
+public class DeeplinkData {
     var sessionToken: String?
     var publicKey: String?
     var selectedBrowser: String?
     var universalLink: String?
     var faceidDuration: String?
     var sessionDuration: String?
+    
+    public init(sessionToken: String? = nil,
+                publicKey: String? = nil,
+                selectedBrowser: String? = nil,
+                universalLink: String? = nil,
+                faceidDuration: String? = nil,
+                sessionDuration: String? = nil) {
+        self.sessionToken = sessionToken
+        self.publicKey = publicKey
+        self.selectedBrowser = selectedBrowser
+        self.universalLink = universalLink
+        self.faceidDuration = faceidDuration
+        self.sessionDuration = sessionDuration
+    }
 }
